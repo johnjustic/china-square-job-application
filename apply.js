8bb2636135ee8d53af0ad3d3d04af04f9a5bc4f1
John <johnjustice766@gmail.com>
2023-11-17 10:22:54 +0300

    done

--- apply.js
+++ apply.js
@@ -35,3 +35,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
     setInterval(typeCaption, 3000); // Change caption every 3 seconds
 });
+// JavaScript to create a typewriter-like effect on the paragraph element
+const text = "Welcome to our team! We're excited to have you join us. Apply now and be part of our success story.";
+let charIndex = 0;
+const typing = () => {
+    const typingText = document.getElementById('typing-effect');
+    if (charIndex < text.length) {
+        typingText.innerHTML += text.charAt(charIndex);
+        charIndex++;
+        setTimeout(typing, 70);
+    }
+}
+window.onload = typing;
+
